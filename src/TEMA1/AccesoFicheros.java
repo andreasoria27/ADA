708560ce89122261bc7b19f06a91730756c75ad0
andsormin <asoriaminguillan@gmail.com>
2024-09-12 18:48:15 +0200

    Inicio ADA

---
+++ src/TEMA1/AccesoFicheros.java
@@ -0,0 +1,50 @@
+package TEMA1;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.List;
+
+public class AccesoFicheros {
+    public static void main(String[] args) {
+    try (FileWriter fw = new FileWriter("files/fichero1.txt")){
+        fw.write("Texto de prueba");
+    } catch (IOException e) {
+        System.err.println("");
+            }
+
+//Files, FIle, Path,Paths
+File file = new File("files/fichero1.txt");
+if (file.exists()){
+    System.out.println("el fichero exite");
+        }
+        System.out.println(file.getAbsoluteFile());
+
+Path p2 = Paths.get("");
+Path path = Path.of("files/fichero1.txt");
+if (Files.exists(path)){
+    System.out.println("EXISTE!!!");
+}
+
+        try {
+            Files.write(path,"Soy ANdrea".getBytes(),StandardOpenOption.APPEND);
+
+            Files.writeString(path,"estoy en clase de ADA", StandardOpenOption.APPEND);
+          List<String> lines = Files.readAllLines(path);
+            System.out.println(lines.size());
+            String textoTotal = Files.readString(path, StandardCharsets.UTF_8);
+            System.out.println(textoTotal);
+
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+    }
+
