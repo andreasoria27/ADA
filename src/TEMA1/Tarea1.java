393c96649148b1f71874a7f0c77878463adefc61
andsormin <asoriaminguillan@gmail.com>
2024-09-12 19:10:17 +0200

    ejercicio2

--- src/TEMA1/Tarea1.java
+++ src/TEMA1/Tarea1.java
@@ -1,13 +1,18 @@
 package TEMA1;
 
 import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.StandardOpenOption;
+import java.util.Scanner;
 
 public class Tarea1 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String linea = sc.nextLine();
         File file = new File("files/fichero1.txt");
         if (file.exists()){
             System.out.println("el fichero existe");
         }
-        System.out.println(file.getAbsoluteFile());
+        Files.writeString(linea, StandardOpenOption.APPEND);
     }
 }
